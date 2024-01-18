606bf7dd26f64b4a16796ff545edda6a0c021e21
SameekshaA20 <sameekshaalawandi2003@gmail.com>
2024-01-18 16:18:11 +0530

    student table created

--- schema.sql
+++ schema.sql
@@ -1,1 +1,10 @@
-create database studentDB;
+CREATE DATABASE IF NOT EXISTS studentDB;
+
+USE studentDB;
+
+CREATE TABLE student (
+    Name varchar(50),
+    college_ID varchar(20),
+    USN varchar(15),
+    primary key(college_ID)
+);
