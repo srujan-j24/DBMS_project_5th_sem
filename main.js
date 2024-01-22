1fb92d61b2b538a4bdecc0012d87ce610dcd36dd
srujan-j24 <srujanj2021@gmail.com>
2024-01-22 19:41:23 +0530

    personal.json addded

--- main.js
+++ main.js
@@ -1,19 +1,16 @@
 const { faker } = require('@faker-js/faker');
 const mysql = require("mysql2");
+const connection_json = require("./personal.json"); //alter this as per your mysql port and password
 
-const connection_pool = mysql.createPool({
-    host: "localhost",
-    user: "root",
-    password: "4922",
-    database: "studentdb"  
-});
+
+const connection_pool = mysql.createPool(connection_json);
 
 let sql_query = 'Show tables;';
 
 connection_pool.query(sql_query, (err, res)=>{
     console.log(res);
-    connection_pool.end();
-})
+
+});
 
 
 
