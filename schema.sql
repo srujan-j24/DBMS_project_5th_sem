43a79447d7831edb7d814e4e29289c9753a8a397
srujan-j24 <srujanj2021@gmail.com>
2024-01-22 18:31:08 +0530

    order updated

--- schema.sql
+++ schema.sql
@@ -2,6 +2,11 @@ CREATE DATABASE IF NOT EXISTS studentDB;
 
 USE studentDB;
 
+CREATE TABLE batch(
+    year int,
+    primary key(year)
+);
+
 CREATE TABLE student (
     Name varchar(50),
     college_ID varchar(20),
@@ -27,40 +32,7 @@ CREATE TABLE marks(
     foreign key(batch_ID) references batch(year) on delete cascade
 );
 
-CREATE TABLE batch(
-    year int,
-    primary key(year)
-);
 
-CREATE TABLE academic_info(
-    college_ID int,
-    scheme_ID varchar(10),
-    batch_ID int,
-    marks_ID varchar(30),
-    sem int,
-    section int,
-    branch varchar(30),
-    primary key(college_ID),
-    foreign key(college_ID) references student(college_ID) on delete cascade,
-    foreign key(scheme_ID) references scheme(scheme_ID) on delete cascade ,
-    foreign key(batch_ID) references batch(year) on delete cascade,
-    foreign key(marks_ID) references marks(marks_ID) on delete cascade,
-    foreign key(sem,section,branch) references class(sem,section,branch) on delete cascade
-);
-
-CREATE TABLE personal_info(
-    college_ID int,
-    blood_type varchar(3),
-    dob date,
-    personal_phone bigint,
-    parent_phone bigint,
-    address varchar(255),
-    validity json,
-    batch_ID int,
-    primary key(college_ID),
-    foreign key(college_ID) references student(college_ID) on delete cascade,
-    foreign key(batch_ID) references batch(year) on delete cascade,
-);
 
 CREATE TABLE scheme(
     scheme_ID varchar(20),
@@ -93,3 +65,34 @@ CREATE TABLE staff(
     password varchar(30),
     primary key(staff_ID)
 );
+
+CREATE TABLE academic_info(
+    college_ID int,
+    scheme_ID varchar(10),
+    batch_ID int,
+    marks_ID varchar(30),
+    sem int,
+    section int,
+    branch varchar(30),
+    primary key(college_ID),
+    foreign key(college_ID) references student(college_ID) on delete cascade,
+    foreign key(scheme_ID) references scheme(scheme_ID) on delete cascade ,
+    foreign key(batch_ID) references batch(year) on delete cascade,
+    foreign key(marks_ID) references marks(marks_ID) on delete cascade,
+    foreign key(sem,section,branch) references class(sem,section,branch) on delete cascade
+);
+
+CREATE TABLE personal_info(
+    college_ID int,
+    blood_type varchar(3),
+    dob date,
+    personal_phone bigint,
+    parent_phone bigint,
+    address varchar(255),
+    validity json,
+    batch_ID int,
+    primary key(college_ID),
+    foreign key(college_ID) references student(college_ID) on delete cascade,
+    foreign key(batch_ID) references batch(year) on delete cascade,
+);
+
