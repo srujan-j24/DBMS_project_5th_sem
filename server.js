b2c1b1bf67f55a40aeb74920087d48d0f2b99266
SameekshaA20 <sameekshaalawandi2003@gmail.com>
2024-02-21 22:23:18 +0530

    connection to server

--- server.js
+++ server.js
@@ -7,7 +7,11 @@ let port = "3000";
 app.use(express.static(path.join(__dirname, "/public/css")));
 app.use(express.static(path.join(__dirname, "/public/js")));
 app.use(express.static(path.join(__dirname, "/public/image")));
+<<<<<<< Updated upstream
 app.use(bodyparser.urlencoded({extended : true}));
+=======
+app.use(bodyparser.urlencoded({extended : true}))
+>>>>>>> Stashed changes
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
@@ -26,6 +30,7 @@ app.get("/staffdashboard",(req,res)=>{
     res.render("staffdashboard.ejs");
 });
 app.post("/login",(req,res)=>{
+<<<<<<< Updated upstream
     //console.log(req.body)
     let username = req.body.username;
     let password = req.body.password;
@@ -40,3 +45,18 @@ app.post("/login",(req,res)=>{
     }
 
 });
+=======
+   // console.log(req.body)
+   let username = req.body.username;
+   let password = req.body.password;
+   //console.log(username);
+   //console.log(password);
+   //console.log(req.body);
+   if(username == "admin"&& password == "admin"){
+        res.send("Successful")
+   }
+   else{
+    res.send("Failure")
+   }
+})
+>>>>>>> Stashed changes
