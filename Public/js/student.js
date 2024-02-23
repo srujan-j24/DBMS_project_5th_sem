5a99bbc3653047032cb13fe5c66faead116b0a2f
SameekshaA20 <sameekshaalawandi2003@gmail.com>
2024-02-23 23:14:43 +0530

    header color

---
+++ Public/js/student.js
@@ -0,0 +1,31 @@
+/*const headers = document.querySelectorAll('.header');*/
+
+const buttons = document.querySelectorAll('.sem-btn');
+
+buttons.forEach(button =>{
+    button.addEventListener('click',()=> {
+        const targetColor = button.getAttribute('background-color');
+        root.style.setProperty('--header-color', 'targetColor');
+    });
+});
+
+       /* headers.forEach(header =>{
+            const headerColor = header.getAttribute('data-color');
+            if(headerColor !== targetColor) {
+                header.style.backgroundColor = `var(--${targetColor})`;
+            }
+        });
+    });
+});*/
+
+ /*function toggleColor() {
+    var div = document.getElementById('toggleDiv');
+    var root = document.documentElement;
+  
+   // Toggle the color value
+    if (root.style.getPropertyValue('--div-color') === 'red') {
+      root.style.setProperty('--div-color', '#ccc'); // Change to default color
+    } else {
+      root.style.setProperty('--div-color', 'red'); // Change to desired color
+    }*/
+  
