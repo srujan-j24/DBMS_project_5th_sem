d5c1083363c5e9f43ad4fb5f92473b11e3aee43c
SameekshaA20 <sameekshaalawandi2003@gmail.com>
2024-03-01 22:36:38 +0530

    basic insertion

---
+++ basic_insertion.sql
@@ -0,0 +1,36 @@
+insert into batch values(2021),(2022);
+
+insert into branch values("CSE","Computer science and engineering"),
+                        ("ISE","Information science and engineering"),
+                        ("ADE","Artificial intelligence engineering"),
+                        ("ECE","Electronics and communication engineering"),
+                        ("EEE","Electrical and electronics engineering"),
+                        ("Civil","Civil engineering");
+
+insert into sem values(1),(2),(3),(4),(5),(6),(7),(8);
+
+insert into scheme values(2021,"CSE"),
+                        (2021,"ISE"),
+                        (2021,"ADE"),
+                        (2021,"ECE"),
+                        (2021,"EEE"),
+                        (2021,"Civil"),
+                        (2022,"CSE"),
+                        (2022,"ISE"),
+                        (2022,"ADE"),
+                        (2022,"ECE"),
+                        (2022,"EEE"),
+                        (2022,"Civil");
+
+insert into class values("CSE_1_A",1,'A',"CSE"),
+                        ("CSE_1_B",1,'B',"CSE");
+
+insert into subjects values("21MAT11",2021,"Calculus and differential equation",3,1),
+                            ("21PHY12",2021,"Engineering physics",3,1),
+                            ("21ELE13",2021,"Basic electrical engineering",3,1),
+                            ("21CIV14",2021,"Elements of civil engineering and mechanics",3,1);
+
+insert into student values(20210001,"Vinod",null,2021,"CSE","CSE_1_A","vin@123",0),
+                        (20210002,"Vihaan",null,2021,"CSE","CSE_1_A","vih@123",0),
+                        (20210003,"Anita",null,2021,"CSE","CSE_1_B","ani@123",0),
+                        (20210004,"Aditi",null,2021,"CSE","CSE_1_B","adi@123",0);
