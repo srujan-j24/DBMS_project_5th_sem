1eac9a6da6ca4dafe4acb6ea2b65b7ef61f6ee4a
SameekshaA20 <sameekshaalawandi2003@gmail.com>
2024-03-02 21:57:22 +0530

    values display

--- Public/js/student.js
+++ Public/js/student.js
@@ -5,6 +5,53 @@ const personal_info_cont = document.getElementById("personal-info-cont");
 const table_cont = document.getElementById("table-container");
 const logout = document.getElementById("logout");
 
+function append_data(data_array) {
+    let table = document.getElementById("table");
+    for(i=0;i<data_array.length;i++) {
+        let {as1,as2,college_ID,credits,ia1,ia2,ia3,name,q1,sub_code} = data_array[i]
+        table.appendChild(create_data_div(sub_code,name));
+        table.appendChild(create_data_div(credits));
+        table.appendChild(create_data_div(" "));
+        table.appendChild(create_data_div(ia1));
+        table.appendChild(create_data_div(ia2));
+        table.appendChild(create_data_div(ia3));
+        let avg = -1;
+        if(ia1!= -1 && ia2!=-1 && ia3!=-1) {
+            avg = sum(ia1,ia2,ia3)/3;
+            table.appendChild(create_data_div(avg));
+        }
+        else {
+            table.appendChild(create_data_div(" "));
+        }
+        table.appendChild(create_data_div(as1));
+        table.appendChild(create_data_div(as2));
+        if(as1!=-1 && as2!=-1) {
+            avg = sum(as1,as2)/2;
+            table.appendChild(create_data_div(avg));
+        }
+        else {
+            table.appendChild(create_data_div(" "));
+        }
+        table.appendChild(create_data_div(q1));
+        table.appendChild(create_data_div(" "));
+        table.appendChild(create_data_div(" "));
+        table.appendChild(create_data_div(" "));
+        table.appendChild(create_data_div(" "));
+    }
+}
+
+function create_data_div(data,additional_attribute){
+    let new_div = document.createElement("div");
+    new_div.classList.add("td");
+    new_div.innerText = data;
+    if(additional_attribute) {
+        new_div.setAttribute("data",additional_attribute);
+    }
+    return new_div;
+}
+
+
+
 buttons.forEach((button,index) =>{
     button.addEventListener('click',()=> {
         const targetColor = window.getComputedStyle(button).backgroundColor;
@@ -37,7 +84,8 @@ buttons.forEach((button,index)=>{
     button.addEventListener("click",()=>{
         $.ajax({type:"POST",url:"/sem",data:{sem:index}})
         .done((response)=>{
-            console.log(response)
+            append_data(response)
+
         })
         .fail((response)=>{
             console.log("error")
