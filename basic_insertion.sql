f10c5187a470010bf544a4f7be4da84d6744f9a2
Vishrutha-VB24 <vishruthavb24@gmail.com>
2024-03-15 11:57:59 +0530

    commit

--- basic_insertion.sql
+++ basic_insertion.sql
@@ -52,7 +52,7 @@
 --  (20210004, "21CIV14", -1, -1, -1, -1, -1, -1);
 
  insert into staff values
--- ("ST_0001","Ramesh T",0,0,"r123@","CSE",0),
+ ("ST_0001","Ramesh T",0,0,"r123@","CSE",0),
 -- ("ST_0002","Suresh U",0,0,"su123@","CSE",0),
    ("ST_0003","Akasha k",1,0,"ak123@","CSE",0);
 
